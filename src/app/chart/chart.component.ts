74411c2c0f468d4d10df36f62a0b0b271302d8bf
matheus.raz <matheus.raz@usto.re>
2019-01-14 18:19:09 -0300

    working flux between boxes

--- src/app/chart/chart.component.ts
+++ src/app/chart/chart.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'chart',
-  templateUrl: './chart.component.html',
+  templateUrl: './chart2.component.html',
   styleUrls: ['./chart.component.scss']
 })
 export class ChartComponent implements OnInit {
@@ -15,6 +15,7 @@ export class ChartComponent implements OnInit {
   pos = [];
   dbclick = 0;
   posis = [];
+  lines = [];
 
 
   
@@ -45,11 +46,13 @@ export class ChartComponent implements OnInit {
   }
 
   checkPosition(){
-    document.getElementsByName('box').forEach((item, key) =>{
-      this.pos[key]= item.getBoundingClientRect();
-      this.apps[`${this.tags[key].id}`].posicao.x = this.pos[key].left;
-      this.apps[`${this.tags[key].id}`].posicao.y = this.pos[key].top;
-    });
+    let lista = document.getElementsByName('box');
+    for(let i=0; i<lista.length;++i){
+      this.pos[i]= lista[i].getBoundingClientRect();
+      this.apps[`${this.tags[i].id}`].posicao.x = this.pos[i].left;
+      this.apps[`${this.tags[i].id}`].posicao.y = this.pos[i].top;
+    }
+    console.log(this.apps);
     
   }
 
@@ -58,18 +61,20 @@ export class ChartComponent implements OnInit {
     this.dbclick++;
     
     if(this.dbclick==2){
-      var canvas = document.getElementsByTagName('canvas');
+      // var canvas = document.getElementsByTagName('canvas');
       // console.log(canvas[0].getContext);
-      var ctx = canvas[0].getContext('2d');
+      // var ctx = canvas[0].getContext('2d');
 
       console.log(this.posis);
   
-      ctx.beginPath();
-      ctx.moveTo(this.posis[0].posicao.x-8,this.posis[0].posicao.y-21);
-      ctx.lineTo(this.posis[1].posicao.x-8,this.posis[1].posicao.y-21);
+      this.lines.push({x1: this.posis[0].posicao.x-12, y1: this.posis[0].posicao.y-25, x2: this.posis[1].posicao.x-12,
+         y2: this.posis[1].posicao.y-25});
+      // ctx.beginPath();
+      // ctx.moveTo(this.posis[0].posicao.x-8,this.posis[0].posicao.y-21);
+      // ctx.lineTo(this.posis[1].posicao.x-8,this.posis[1].posicao.y-21);
       // ctx.moveTo(0,0);
       // ctx.lineTo(0,100);
-      ctx.stroke();
+      // ctx.stroke();
 
       this.tags[this.apps[`${tag}`].index].disabled=true;
       this.tags[this.apps[`${this.posis[0].id}`].index].disabled=true;
@@ -84,12 +89,21 @@ export class ChartComponent implements OnInit {
   }
 
   apagaLig(){
-    var canvas = document.getElementsByTagName('canvas');
-    var ctx = canvas[0].getContext('2d');
-    ctx.clearRect(0, 0, 300, 150);
-    for(let i=0;i<this.tam;i++){
-      this.apps[`${this.tags[i].id}`].disabled=false;
-    }
+    // var canvas = document.getElementsByTagName('canvas');
+    // var ctx = canvas[0].getContext('2d');
+    // ctx.clearRect(0, 0, 300, 150);
+    // for(let i=0;i<this.tam;i++){
+    //   this.apps[`${this.tags[i].id}`].disabled=false;
+    // }
+    this.box = '';
+    this.apps = {};
+    this.seta = false;
+    this.tam = 0;
+    this.tags = [];
+    this.pos = [];
+    this.dbclick = 0;
+    this.posis = [];
+    this.lines = [];
   }
 
 }
--- src/app/chart/chart.module.ts
+++ src/app/chart/chart.module.ts
@@ -1,13 +1,19 @@
 import { ChartComponent } from './chart.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {DragDropModule} from '@angular/cdk/drag-drop';
-
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { SvgCircleModule, SvgLineModule, SvgPolygonModule, SvgPolylineModule, SvgTextModule, SvgPathModule } from 'angular-svg'
 
 @NgModule({
   imports: [
     CommonModule,
-    DragDropModule
+    DragDropModule,
+    SvgCircleModule,
+    SvgLineModule,
+    SvgPolygonModule,
+    SvgPolylineModule,
+    SvgTextModule,
+    SvgPathModule,
   ],
   declarations: [ChartComponent],
   exports: [ChartComponent]
