f1f152b5661934fd6efc8913fa0ca3dade3b5dd6
Lucas Grisi <lucasgrisii@gmail.com>
2019-01-14 12:10:10 -0300

    travando apos ligacao

--- src/app/chart/chart.component.ts
+++ src/app/chart/chart.component.ts
@@ -24,8 +24,12 @@ export class ChartComponent implements OnInit {
   }
 
   addBox(){
-    this.tags.push(this.box);
+    this.tags[this.tam] = {
+      id:this.box,
+      disabled:false
+    };
     this.apps[`${this.box}`] = {
+      index:this.tam,
       id: this.box,
       posicao: {
         x : null,
@@ -33,6 +37,7 @@ export class ChartComponent implements OnInit {
       },
       ligacao : null
     };
+    this.tam++;
   }
 
   updateBox(event){
@@ -42,8 +47,8 @@ export class ChartComponent implements OnInit {
   checkPosition(){
     document.getElementsByName('box').forEach((item, key) =>{
       this.pos[key]= item.getBoundingClientRect();
-      this.apps[`${this.tags[key]}`].posicao.x = this.pos[key].left;
-      this.apps[`${this.tags[key]}`].posicao.y = this.pos[key].top;
+      this.apps[`${this.tags[key].id}`].posicao.x = this.pos[key].left;
+      this.apps[`${this.tags[key].id}`].posicao.y = this.pos[key].top;
     });
     
   }
@@ -56,6 +61,8 @@ export class ChartComponent implements OnInit {
       var canvas = document.getElementsByTagName('canvas');
       // console.log(canvas[0].getContext);
       var ctx = canvas[0].getContext('2d');
+
+      console.log(this.posis);
   
       ctx.beginPath();
       ctx.moveTo(this.posis[0].posicao.x-8,this.posis[0].posicao.y-21);
@@ -64,6 +71,8 @@ export class ChartComponent implements OnInit {
       // ctx.lineTo(0,100);
       ctx.stroke();
 
+      this.tags[this.apps[`${tag}`].index].disabled=true;
+      this.tags[this.apps[`${this.posis[0].id}`].index].disabled=true;
       this.apps[`${tag}`].ligacao = this.posis[0].id;
       this.apps[`${this.posis[0].id}`].ligacao = this.posis[1].id;
 
